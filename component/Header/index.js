a6a464c5dc598a658e9610d7c3318e629b660411
yogitaba <yogitag127@hupp.in>
2022-10-17 02:39:48 +0300

    mobile view done

--- component/Header/index.js
+++ component/Header/index.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/dist/client/image'
 import styles from '../../styles/Header.module.scss'
 import Link from 'next/link'
 
 const Header = () => {
+  const [active,setActive]= useState(false);
+
+  const handleMenu = () =>{
+    setActive(!active);
+  }
   return (
     <div className={styles.container}>
-    <div className={styles.logoContainer}><Image alt='logo image'  src="/assets/logo.svg" width="200px" height="100px" />
+    <div className={styles.logoContainer}><Image alt='logo image'  src="/assets/logo.svg" width="180px" height="80px" />
     </div>
     <div className={styles.menuItem}>
     
@@ -15,10 +20,26 @@ const Header = () => {
       <div><Link href="/">Blog </Link></div>
     </div>
     <div className={styles.menuContainer}>
-      <button>
+      <button onClick={handleMenu}>
       <Image src="/assets/menu.svg" alt='menu icon' width="50px" height="30px"/>
       </button>
+      
+    </div> 
+    {active ? <div className={styles.mobileHeader}> 
+    
+      <div className={styles.mobileCotainer}>
+
+      <div><Image alt='logo image'  src="/assets/logo.svg" width="100px" height="70px" /></div>
+      <div className={styles.closeContainer}><button onClick={handleMenu}><Image alt='logo image'  src="/assets/close.svg" width="60px" height="20px" /> </button></div>
+      
     </div>
+    <div>
+    <div className={styles.linkContainer}><Link href="/">Chrome Extension</Link></div>
+    <div className={styles.linkContainer}><Link href="/">Price Comparison</Link></div>
+    <div className={styles.linkContainer}><Link href="/">Blog</Link></div>
+    </div>
+    </div> : null}
+
     </div>
   )
 }
